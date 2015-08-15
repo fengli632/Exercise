27d868322a805d790300a20f51d7fe218d51a2c1
FengLi <lifeng632@163.com>
2015-08-16 00:26:06 +0800

    修改sqlLite-web留言本 removeAllData() bug

--- webStorage/js/j4.js
+++ webStorage/js/j4.js
@@ -1,5 +1,5 @@
 var datatable = null;
-var db = opeDatabase("MyData","","My Database desc",1024*100);
+var db = openDatabase("MyData","","My Database desc",1024*100);
 function init(){
     datatable = document.getElementById("datatable");
     showAllData();
@@ -16,6 +16,9 @@ function removeAllData(){
         th1.innerHTML = "name";
         th2.innerHTML = "memo";
         th3.innerHTML = "time";
+        tr.appendChild(th1);
+        tr.appendChild(th2);
+        tr.appendChild(th3);
         datatable.appendChild(tr);
 }
 
@@ -37,54 +40,34 @@ function showData(row){
 
 function showAllData(){
     db.transaction(function(tx){
-        tx.executeSql("create table if not exists MsgData(name TEXT,message TEXT,time INTEGER)",[]);
-        tx.executeSql("select * from MsgData",[],function(tx,rs){
-
+        tx.executeSql("CREATE TABLE IF NOT EXISTS MsgData(name TEXT,message TEXT,time INTEGER)",[]);
+        tx.executeSql("SELECT * FROM MsgData",[],function (tx,rs) {
             removeAllData();
-
             for(var i=0;i<rs.rows.length;i++){
-
-                showData(rs.rows.item(i))
-
+                showData(rs.rows.item(i));
             }
-
         })
-    });
+        
+    })
 }
 
 function addData(name,message,time){
-
     db.transaction(function(tx){
-
-        tx.executeSql("insert into MsgData VALUES(?,?,?)",[name,message,time],function(tx,rs){
-
-            alert("成功");
-
-        },
-
-          function(tx,error){
-
-              alert(error.source+"::"+error.message);
-
-          }
-
-        )
-
+        tx.executeSql("INSERT INTO MsgData VALUES(?,?,?)",
+            [name,message,time],
+            function(){
+                alert("添加成功！");
+            },
+            function(tx,error){
+                alert(error.source+"::"+error.message);
+             })
     })
-
 }
 
 function saveData(){
-
-    var name=document.getElementById("name").value();
-
-    var memo=document.getElementById("memo").value();
-
-    var time=new Date().getTime();
-
+    var name = document.getElementById("name").value;
+    var memo = document.getElementById("memo").value;
+    var time = new Date().getTime();
     addData(name,memo,time);
-
     showAllData();
-
 }
-
